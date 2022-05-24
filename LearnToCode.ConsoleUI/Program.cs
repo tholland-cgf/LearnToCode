f89f29093a672ae7f096c64b73223657a4526026
Holland, Terry <tholland@cgf.com>
2022-05-24 08:59:29 +0100

    Add project files.

---
+++ LearnToCode.ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LearnToCode.ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            Hello("John");
+            Hello("Terry");
+        }
+
+
+        static void Hello(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
