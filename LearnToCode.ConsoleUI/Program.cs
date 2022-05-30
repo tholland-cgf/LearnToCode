60ba7f585d1ae21cd94243f8678b0de61c4eee06
Holland, Terry <tholland@cgf.com>
2022-05-30 08:32:43 +0100

    Added some methods to demonstrate Lists

--- LearnToCode.ConsoleUI/Program.cs
+++ LearnToCode.ConsoleUI/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace LearnToCode.ConsoleUI
 {
@@ -6,8 +9,66 @@ namespace LearnToCode.ConsoleUI
     {
         static void Main(string[] args)
         {
-            InputTest();
+            ReadPersistedListTest();
+            //ListTest();
+            //InputTest();
             Console.ReadLine();
+        }
+
+        private static void ListTest()
+        {
+            List<string> names = new List<string>();
+
+
+            Console.WriteLine("Enter a name");
+            //get name from user
+            string name1 = Console.ReadLine();
+            //and add to list
+            names.Add(name1);
+
+
+            Console.WriteLine("And another...");
+            //less code
+            //get next name from user
+            names.Add(Console.ReadLine());
+
+
+            Console.WriteLine("And one more...");
+            //...and another
+            names.Add(Console.ReadLine());
+
+
+
+            Console.WriteLine("Printing the list of names in the same order they were entered");
+            //print out the name in the order they were added
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            Console.WriteLine("Printing the list of names in the alpha order");
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        private static void ReadPersistedListTest()
+        {
+            List<string> names = new List<string>();
+            var existingNames = File.ReadAllLines("Names.txt");
+
+            foreach (var name in existingNames)
+            {
+                Console.WriteLine(name);
+                names.Add(name);
+            }
+
+
+            
+
         }
 
         private static void InputTest()
@@ -40,3 +101,4 @@ namespace LearnToCode.ConsoleUI
 
     }
 }
+
