4b53a773e2a2a7cd25352e7b136ce3fa2d072b95
GregD1313 <greg.dixon85@outlook.com>
2022-07-11 11:14:10 +0100

    sudo code added

--- LearnToCode.Repository/PersonFileRepository.cs
+++ LearnToCode.Repository/PersonFileRepository.cs
@@ -28,12 +28,34 @@ namespace LearnToCode.Repository
             List<Person> list = new List<Person>();
             string _filepath;
             _filepath = @"\\ldn-cgfiles\General\Greg\LearnToCode\Files\Names.txt";
-            static IEnumerable<string> GetRecordsFromFile(string filepath)
-            {
-                return File.ReadAllLines(filepath);
-            }
-            List<string> records = GetRecordsFromFile(_filepath).ToList();
             
+            string[] records = File.ReadAllLines(_filepath);
+            char[] delimiterChars = { ' ',',' };
+
+            //Start of Sudo Code for file reading 
+
+            //start of loop to go through records array (created buy reading contents of file) loop to carry on as long as there are lines in records.
+            //take first line of array "record" 
+            //split up the string at the break point (code can be heard coded to use either tab [' '] or a comma [,] or even both) 
+                //take value before first break convert to integer and add as " p.Id = Convert.ToInt32(dr["Id"]);"
+                //take value after first break "convert" to a string and add as "p.Name = Convert.ToString(dr["Name"]);"
+                //take value after second break "Convert to interger and add as "p.Age = Convert.ToInt32(dr["Age"]);
+                //add person p to the list 
+            //close the loop 
+            //return list 
+
+
+
+
+
+            //while ((ln = records.ReadLine()) !=null)
+            //{
+            //  Console.WriteLine(ln);
+            //}
+
+
+
+            //List<string> records = GetRecordsFromFile(_filepath).ToList();
 
 
             //using (StreamReader dr = new StreamReader(_filepath))
@@ -48,7 +70,7 @@ namespace LearnToCode.Repository
             //list.Add(p)
             //}
             //}
-            
+
 
             return list;
             
